9e3aa2eff42dbf1a95faa7b4da734aab39d106b1
PieroVicenti <p.vicenti@outlook.com>
2022-01-08 15:43:55 +0000

    Updates

---
+++ precache-manifest.4438f0236608b3a7e1eeea8837386bf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d3115e918e1f34f685da9a92a3c4fe",
+    "url": "/Tours/index.html"
+  },
+  {
+    "revision": "a103e84f4ed075497bec",
+    "url": "/Tours/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/Tours/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tours/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a103e84f4ed075497bec",
+    "url": "/Tours/static/js/main.cf3a5707.chunk.js"
+  },
+  {
+    "revision": "3128dff8851166541ea2",
+    "url": "/Tours/static/js/runtime-main.83f56e71.js"
+  }
+]);
